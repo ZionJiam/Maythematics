262a625434c2d502d8dbb5138337756d61332267
ZionJiam <52207958+ZionJiam@users.noreply.github.com>
2025-08-06 14:52:19 +0100

    No Crawl Index

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,21 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  },
 };
-
 
 export default nextConfig;
