8673d348887cce0306d7ec488fc1da5e04eb4baf
ZionJiam <52207958+ZionJiam@users.noreply.github.com>
2025-09-24 23:05:52 +0800

    Insert Google Analytics 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,33 @@
+'use client'
+
+// app/layout.tsx
+import Script from "next/script";
+import { usePathname } from "next/navigation";
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+    const pathname = usePathname();
+    const isAdmin = pathname?.startsWith("/admin");
+
     return (
         <html lang="en">
             <body>
-                {children} {/* Public and Admin layouts will render here */}
+                {!isAdmin && (
+                    <Script
+                        src={`https://www.googletagmanager.com/gtag/js?id=G-HXDQZW4PEE`}
+                        strategy="afterInteractive"
+                    />
+                )}
+                {!isAdmin && (
+                    <Script id="ga-init" strategy="afterInteractive">
+                        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-HXDQZW4PEE');
+            `}
+                    </Script>
+                )}
+                {children}
             </body>
         </html>
     );
