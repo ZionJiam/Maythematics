279b83b0a3fe0b3844785c0f87e72cb41c324af9
ZionJiam <52207958+ZionJiam@users.noreply.github.com>
2026-06-14 19:41:45 +0800

    Update GTag to deal with ad blockers

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -6,10 +6,14 @@ declare global {
 }
 
 export const trackCTA = (label: string) => {
-  if (typeof window.gtag !== 'undefined') {
-    window.gtag('event', 'cta_click', {
-      event_category: 'CTA Button',
-      event_label: label,
-    })
+  try {
+    if (typeof window.gtag === 'function') {
+      window.gtag('event', 'cta_click', {
+        event_category: 'CTA Button',
+        event_label: label,
+      });
+    }
+  } catch (e) {
+    // GA blocked, silently ignore
   }
 }
